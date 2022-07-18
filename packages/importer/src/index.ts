ebed5e69b0d9ed5cadbc9b1ffb3eb6087a0a0df8
Ryan Patterson <ryapapap@gmail.com>
2022-07-18 10:26:11 -0700

    Fix serverside rendering (#11)
    * fix

--- packages/importer/src/index.ts
+++ packages/importer/src/index.ts
@@ -32,18 +32,22 @@ const DEFAULT_ONESCHEMA_OPTIONS: OneSchemaImporterConfigOptions = {
 
 class OneSchemaImporter extends EventEmitter {
   clientId: string
-  iframe: HTMLIFrameElement
+  iframe?: HTMLIFrameElement
   iframeConfig: OneSchemaIframeConfig
   #baseUrl = "https://embed.oneschema.co"
-  #eventListener: (event: MessageEvent) => void
-  #isLoaded = false
+  #eventListener: (event: MessageEvent) => void = () => null
+  static #isLoaded = false
 
   #hide() {
-    this.iframe.style.display = "none"
+    if (this.iframe) {
+      this.iframe.style.display = "none"
+    }
   }
 
   #show() {
-    this.iframe.style.display = "initial"
+    if (this.iframe) {
+      this.iframe.style.display = "initial"
+    }
   }
 
   #getParent(): HTMLElement {
@@ -70,29 +74,33 @@ class OneSchemaImporter extends EventEmitter {
       this.#baseUrl = baseUrl
     }
 
-    const iframeId = "_oneschema-iframe"
-
-    this.iframe =
-      (document.getElementById(iframeId) as HTMLIFrameElement) ||
-      document.createElement("iframe")
-    this.iframe.id = iframeId
-    if (this.iframe.dataset.count) {
-      this.iframe.dataset.count = `${parseInt(this.iframe.dataset.count) + 1}`
-    } else {
-      this.iframe.dataset.count = "1"
+    if (typeof window === "undefined") {
+      return
     }
 
-    const queryParams = `?embed_client_id=${this.clientId}&dev_mode=${this.iframeConfig.devMode}`
-    this.iframe.src = `${this.#baseUrl}/embed-launcher${queryParams}`
-    this.iframe.className = this.iframeConfig.className || ""
-    this.iframe.onload = () => {
-      this.#isLoaded = true
+    const iframeId = "_oneschema-iframe"
+
+    this.iframe = document.getElementById(iframeId) as HTMLIFrameElement
+    if (!this.iframe) {
+      this.iframe = document.createElement("iframe")
+      this.iframe.id = iframeId
+      this.iframe.dataset.count = "1"
+
+      const queryParams = `?embed_client_id=${this.clientId}&dev_mode=${this.iframeConfig.devMode}`
+      this.iframe.src = `${this.#baseUrl}/embed-launcher${queryParams}`
+      this.iframe.className = this.iframeConfig.className || ""
+      OneSchemaImporter.#isLoaded = false
+      this.iframe.onload = () => {
+        OneSchemaImporter.#isLoaded = true
+      }
+    } else {
+      this.iframe.dataset.count = `${parseInt(this.iframe.dataset.count || "0") + 1}`
     }
 
     this.#hide()
 
     this.#eventListener = (event: MessageEvent) => {
-      if (event.source !== this.iframe.contentWindow) {
+      if (event.source !== this.iframe?.contentWindow) {
         return
       }
 
@@ -133,7 +141,7 @@ class OneSchemaImporter extends EventEmitter {
 
     const { options, ...rest } = config
     const postInit = () => {
-      this.iframe.contentWindow?.postMessage(
+      this.iframe?.contentWindow?.postMessage(
         {
           messageType: "init",
           ...rest,
@@ -146,22 +154,22 @@ class OneSchemaImporter extends EventEmitter {
       )
     }
 
-    if (this.#isLoaded) {
+    if (OneSchemaImporter.#isLoaded) {
       postInit()
-    } else {
+    } else if (this.iframe) {
       this.iframe.onload = postInit
-      this.#isLoaded = true
+      OneSchemaImporter.#isLoaded = true
     }
   }
 
   close(clean?: boolean) {
-    if (this.#isLoaded) {
+    if (this.iframe && OneSchemaImporter.#isLoaded) {
       this.iframe.contentWindow?.postMessage({ messageType: "close" }, this.#baseUrl)
     }
 
     this.#hide()
 
-    if (clean) {
+    if (clean && this.iframe) {
       if (this.iframe.dataset.count === "1") {
         this.iframe.remove()
         this.removeAllListeners()
