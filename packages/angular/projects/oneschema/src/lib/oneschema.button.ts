5c7a860c08b390f52f76581d0c4de91c60879d44
Ryan Patterson <ryapapap@gmail.com>
2022-07-26 15:17:15 -0700

    better button

--- packages/angular/projects/oneschema/src/lib/oneschema.button.ts
+++ packages/angular/projects/oneschema/src/lib/oneschema.button.ts
@@ -2,14 +2,10 @@ import { Component } from '@angular/core'
 import { OneSchemaService } from './oneschema.service'
 
 @Component({
-  selector: 'oneschema-button',
-  template: `<button (click)="launch()">Open OneSchema</button>`,
+  selector: 'lib-oneschema-button',
+  template: `<button (click)="oneschema.launch()">Open OneSchema</button>`,
   styles: [],
 })
 export class OneSchemaButton {
   constructor(public oneschema: OneSchemaService) {}
-
-  launch() {
-    this.oneschema.launch()
-  }
 }
