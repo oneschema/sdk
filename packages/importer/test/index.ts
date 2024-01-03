779d524ff2f17608fd5500ae2daccd6e927d80de
Michael Zhao <michael.zhao@gmail.com>
2024-01-03 10:32:35 -0800

    undo test changes

--- packages/importer/test/index.ts
+++ packages/importer/test/index.ts
@@ -10,15 +10,9 @@ const importer = oneSchemaImporter({
     "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI4MDMyYWY0Yi1lMmQ5LTQwYWQtODE0Mi1lNjgwMTFkOTRkOWMiLCJ1c2VyX2lkIjoiPFVTRVJfSUQ-In0.UHcGqI6HutinJNCxO32dWNDBbfUJVVqrvmA_GLpLf50",
   devMode: true,
 })
-let customizationOverrides = {}
 
 function start() {
-  importer.launch(customizationOverrides)
-  setTimeout(() => {
-    customizationOverrides = {
-      backgroundPrimaryColor: "#FFAAFF",
-    }
-  }, 5000)
+  importer.launch()
 }
 
 importer.on("launched", (data) => {
