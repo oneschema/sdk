a1899c9730795f1e3b4c387201c95ba494d5378c
Michael Zhao <michael.zhao@gmail.com>
2024-01-10 16:50:09 -0800

    fix vue test (#86)

--- packages/vue/test/OneSchemaImporter.vue
+++ packages/vue/test/OneSchemaImporter.vue
@@ -1,38 +1,38 @@
 <script setup lang="ts">
-  import { useOneSchemaImporter } from "../src"
+import { useOneSchemaImporter } from "../src"
 
-  const importer = useOneSchemaImporter();
+const importer = useOneSchemaImporter()
 
-  const launchOneSchema = function () {
-    importer.launch();
+const launchOneSchema = function () {
+  importer.launch()
+}
 
-    importer.on('success', (data) => {
-      // TODO: handle success
-      console.log(data);
-    });
+importer.on("success", (data) => {
+  // TODO: handle success
+  console.log(data)
+})
 
-    importer.on('cancel', () => {
-      // TODO: handle cancel
-    });
+importer.on("cancel", () => {
+  // TODO: handle cancel
+})
 
-    importer.on('error', (error) => {
-      // TODO: handle errors
-      console.log(message);
-    });
-  };
+importer.on("error", (error) => {
+  // TODO: handle errors
+  console.log(error)
+})
 </script>
 <template>
   <button @click="launchOneSchema">Launch embed</button>
 </template>
 
 <style>
-  .oneschema-iframe {
-    width: 100vw;
-    height: 100vh;
-    border: none;
-    position: absolute;
-    right: 0;
-    top: 0;
-    z-index: 10000; /* adjust as needed */
-  }
-</style> 
+.oneschema-iframe {
+  width: 100vw;
+  height: 100vh;
+  border: none;
+  position: absolute;
+  right: 0;
+  top: 0;
+  z-index: 10000; /* adjust as needed */
+}
+</style>
