99dcfce766ee277d66d3b808ce3f4e860701603f
Jenn Kao <jennkao92@gmail.com>
2024-06-27 10:52:39 -0700

    Remove updates to the vue package

--- packages/vue/rollup.config.js
+++ packages/vue/rollup.config.js
@@ -3,40 +3,32 @@ import commonjs from "@rollup/plugin-commonjs"
 import json from "@rollup/plugin-json"
 import typescript from "rollup-plugin-typescript2"
 import external from "rollup-plugin-peer-deps-external"
-import dts from "rollup-plugin-dts"
 
-export default [
-  {
-    input: ["src/index.ts"],
-    plugins: [
-      external(),
-      nodeResolve(),
-      commonjs(),
-      json(),
-      typescript({
-        tsconfigOverride: {
-          exclude: ["**/test"],
-        },
-      }),
-    ],
-    output: [
-      {
-        file: "dist/module.js",
-        format: "esm",
-        exports: "named",
-        sourcemap: true,
+export default {
+  input: ["src/index.ts"],
+  plugins: [
+    external(),
+    nodeResolve(),
+    commonjs(),
+    json(),
+    typescript({
+      tsconfigOverride: {
+        exclude: ["**/test"],
       },
-      {
-        file: "dist/main.js",
-        format: "cjs",
-        exports: "named",
-        sourcemap: true,
-      },
-    ],
-  },
-  {
-    input: ["src/index.ts"],
-    output: [{ file: "dist/index.d.ts", format: "es" }],
-    plugins: [dts()],
-  },
-]
+    }),
+  ],
+  output: [
+    {
+      file: "dist/module.js",
+      format: "esm",
+      exports: "named",
+      sourcemap: true,
+    },
+    {
+      file: "dist/main.js",
+      format: "cjs",
+      exports: "named",
+      sourcemap: true,
+    },
+  ],
+}
--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,11 +1,11 @@
-import { OneSchemaImporterClass, OneSchemaParams } from "@oneschema/importer"
-import { OneSchemaPlugin } from "./plugin"
+import { OneSchemaImporterClass, OneSchemaParams } from "@oneschema/importer";
+import { OneSchemaPlugin } from "./plugin";
 
-import { inject } from "vue"
+import { inject } from "vue";
 
-export * from "@oneschema/importer"
-
-export function createOneSchemaImporter(createParams: OneSchemaParams) {
+export function createOneSchemaImporter(
+  createParams: OneSchemaParams
+) {
   return new OneSchemaPlugin(createParams)
 }
 
